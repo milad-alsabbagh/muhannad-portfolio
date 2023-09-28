2bf8dcd65dc6543db5a8ea340c6941ff373f3b38
mimo-404nf <mimo@404notfound.group>
2023-09-28 14:41:33 -0400

    Add some skills

--- lib/models_lists/skill_list.dart
+++ lib/models_lists/skill_list.dart
@@ -1,6 +1,20 @@
 import '../models/skill_model.dart';
-int currentIndex=-1;
-List<SkillModel> skillsList=[
-  SkillModel(index: ++currentIndex, proficiency: 80, skill: 'c++', yearOfXp: 3),
-  SkillModel(index: ++currentIndex, proficiency: 80, skill: 'c++', yearOfXp: 3),
+
+int currentIndex = -1;
+List<SkillModel> skillsList = [
+  SkillModel(
+      index: ++currentIndex, proficiency: 90, skill: 'Python', yearOfXp: 3),
+  SkillModel(
+      index: ++currentIndex, proficiency: 50, skill: 'Docker', yearOfXp: 3),
+  SkillModel(
+      index: ++currentIndex, proficiency: 50, skill: 'Kubernetes', yearOfXp: 3),
+  SkillModel(
+      index: ++currentIndex, proficiency: 75, skill: 'Django', yearOfXp: 3),
+  SkillModel(
+      index: ++currentIndex, proficiency: 95, skill: 'IT.Support', yearOfXp: 5),
+  SkillModel(
+      index: ++currentIndex,
+      proficiency: 90,
+      skill: 'Project Management',
+      yearOfXp: 5),
 ];
